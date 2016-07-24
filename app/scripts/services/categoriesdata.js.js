94c81eddadd42790d837eddbd565c92a697862b5
Sandoche Adittane <sandoche.adittane@nl.ibm.com>
2016-07-24 14:06:42 +0200

    services created

---
+++ app/scripts/services/categoriesdata.js.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name pokestackApp.categoriesData.js
+ * @description
+ * # categoriesData.js
+ * Value in the pokestackApp.
+ */
+angular.module('pokestackApp')
+  .value('categoriesData.js', 42);
---
+++ app/scripts/services/searchservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name pokestackApp.searchService
+ * @description
+ * # searchService
+ * Service in the pokestackApp.
+ */
+angular.module('pokestackApp')
+  .service('searchService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
