fee6943e09f3a95c797df704d0bcf09e3a5211e2
Sandoche Adittane <sandoche.adittane@nl.ibm.com>
2016-07-24 14:43:33 +0200

    routes defined

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,21 +20,11 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/home', {
         templateUrl: 'views/home.html',
         controller: 'HomeCtrl',
         controllerAs: 'home'
       })
-      .when('/category', {
+      .when('/:categoryName', {
         templateUrl: 'views/category.html',
         controller: 'CategoryCtrl',
         controllerAs: 'category'
