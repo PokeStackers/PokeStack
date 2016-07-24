50bc8f912d95b580e293ef3f92c82eaf15f1005b
Sandoche Adittane <sandoche.adittane@nl.ibm.com>
2016-07-24 13:49:14 +0200

    Boiler plate generated with yeoman generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pokestackApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pokestackApp
+ */
+angular.module('pokestackApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pokestackApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pokestackApp
+ */
+angular.module('pokestackApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
